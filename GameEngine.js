2f31876471f0506059bebd0d967cb07160d268c7
VivienParsis <94974717+Vivien-Parsis@users.noreply.github.com>
2022-11-28 10:17:12 +0000

    v1.1

--- GameEngine.js
+++ GameEngine.js
@@ -8,12 +8,16 @@ var life = 3;
 var speedObstacle = 200;
 var speedEnfant = 150;
 var score = 0;
+var HarponActive = false;
+var OursinActive = true;
+var highScore = 0;
 
 function preload() {
     this.load.image('player', 'assets/player.png');
-    this.load.image('obstacle', 'assets/oursin.png');
+    this.load.image('oursin', 'assets/oursin.png');
     this.load.image('background', 'assets/background.png');
     this.load.image('enfant', 'assets/bg.png');
+    this.load.image('harpon', 'assets/harpon.png');
 }
 
 function create() {
@@ -29,34 +33,85 @@ function create() {
     this.player.setCollideWorldBounds(true);
     this.player.setImmovable(true);
   
-    this.obstacle = this.physics.add.image(config.width / 2, 50, 'obstacle').setScale(0.07, 0.07);
+    this.oursin = this.physics.add.image(config.width / 2, 50, 'oursin').setScale(0.07, 0.07);
     this.enfant = this.physics.add.image(config.width / 2, 50, 'enfant').setScale(0.20, 0.20);
-    this.obstacle.setPosition(RandInt(this.obstacle.displayWidth/2,config.width - (this.obstacle.displayWidth/2)), -50);
+    this.harpon = this.physics.add.image(config.width / 2, 50, 'harpon').setScale(0.5, 0.5);
     this.enfant.setPosition(RandInt(this.enfant.displayWidth/2,config.width - (this.enfant.displayWidth/2)), -50);
     
+    do{this.oursin.setPosition(RandInt(this.oursin.displayWidth/2,config.width - (this.oursin.displayWidth/2)), -150);}
+      while((this.oursin.x-(this.oursin.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.oursin.x-(this.oursin.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2)) 
+            || (this.oursin.x+(this.oursin.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.oursin.x+(this.oursin.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2)));
     
+    do{this.harpon.setPosition(RandInt(this.harpon.displayWidth/2,config.width - (this.harpon.displayWidth/2)), -150);}
+      while((this.harpon.x-(this.harpon.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.harpon.x-(this.harpon.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2)) 
+            || (this.harpon.x+(this.harpon.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.harpon.x+(this.harpon.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2)));
 
     this.chronoText = this.add.text(10,10,'chrono',{fontfamily:"Passion-Regu",fill:'#dddddd',stroke:'#000000',strokeThickness:5});
-    this.win = this.add.text(config.width/3,75,'',{fontfamily:"Passion-Regu",fill:'#eeee00',stroke:'#222222',strokeThickness:6});
+    this.win = this.add.text(config.width/3,config.height/2,'',{fontfamily:"Passion-Regu",fill:'#eeee00',stroke:'#222222',strokeThickness:6});
+    
 
     this.timer = this.time.delayedCall(1000,onEvent,null,this);
-    this.physics.add.collider
+    
+    this.ColisionPlOr = this.physics.add.collider
       (
-          this.player,
-          this.obstacle,
-          function(_player,_obstacle)
+        this.player,
+        this.oursin,
+        function(_player,_oursin)
         {
-          if(_player.body.touching.up && _obstacle.body.touching.down
-             || _player.body.touching.left && _obstacle.body.touching.right
-             || _player.body.touching.right && _obstacle.body.touching.left)
+          if(_player.body.touching.up && _oursin.body.touching.down
+             || _player.body.touching.left && _oursin.body.touching.right
+             || _player.body.touching.right && _oursin.body.touching.left)
           {
             life -= 1;
-            _obstacle.setPosition(RandInt(_obstacle.displayWidth/2,config.width - (_obstacle.displayWidth/2)), -50);
+            _oursin.setPosition(RandInt(_oursin.displayWidth/2,config.width - (_oursin.displayWidth/2)), -50);
             speedObstacle += 10;
+            let randSelect = RandInt(0,2);
+            if(randSelect==0)
+            {
+              HarponActive = true;
+              OursinActive = false;
+            }
+            if(randSelect==1)
+            {
+              HarponActive = false;
+              OursinActive = true;
+            }
+            
           }
         }
       );
-    this.physics.add.collider
+    this.ColisionPlHa = this.physics.add.collider
+      (
+        this.player,
+        this.harpon,
+        function(_player,_harpon)
+        {
+          if(_player.body.touching.up && _harpon.body.touching.down
+             || _player.body.touching.left && _harpon.body.touching.right
+             || _player.body.touching.right && _harpon.body.touching.left)
+          {
+             life -= 1;
+            _harpon.setPosition(RandInt(_harpon.displayWidth/2,config.width - (_harpon.displayWidth/2)), -50);
+            speedObstacle += 10;
+            let randSelect = RandInt(0,2);
+            if(randSelect==0)
+            {
+              HarponActive = true;
+              OursinActive = false;
+            }
+            if(randSelect==1)
+            {
+              HarponActive = false;
+              OursinActive = true;
+            }   
+          }
+        }
+      );
+    this.ColisionPlEf = this.physics.add.collider
       (
           this.player,
           this.enfant,
@@ -75,8 +130,15 @@ function create() {
 }
 
 function update() {
-    this.obstacle.setVelocityX(0);
+    this.oursin.setVelocityX(0);
     this.enfant.setVelocityX(0);
+    this.harpon.setVelocityX(0);
+    
+    this.oursin.setActive(OursinActive).setVisible(OursinActive);
+    this.ColisionPlOr.active = OursinActive;
+    this.harpon.setActive(HarponActive).setVisible(HarponActive);
+    this.ColisionPlHa.active = HarponActive;
+    
     if(this.r.isDown)
     {
       pause=false;
@@ -88,33 +150,91 @@ function update() {
       speedEnfant = 200;
       score = 0;
       this.player.setVelocityX(0);
-      this.obstacle.setPosition(RandInt(this.obstacle.displayWidth/2,config.width - (this.obstacle.displayWidth/2)), -50);
       this.enfant.setPosition(RandInt(this.enfant.displayWidth/2,config.width - (this.enfant.displayWidth/2)), -50);
       this.player.setPosition(config.width / 2, config.height - (this.player.displayHeight/2));
       
+      do{this.oursin.setPosition(RandInt(this.oursin.displayWidth/2,config.width - (this.oursin.displayWidth/2)), -150);}
+        while((this.oursin.x-(this.oursin.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.oursin.x-(this.oursin.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2)) 
+            || (this.oursin.x+(this.oursin.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.oursin.x+(this.oursin.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2)));
+    
+      do{this.harpon.setPosition(RandInt(this.harpon.displayWidth/2,config.width - (this.harpon.displayWidth/2)), -150);}
+        while((this.harpon.x-(this.harpon.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.harpon.x-(this.harpon.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2)) 
+            || (this.harpon.x+(this.harpon.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.harpon.x+(this.harpon.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2))); 
     }
   
     if(pause==false)
     {
-      this.chronoText.setText('⏲timer: '+timeCount+'s\n❤️PV: '+life+'\nScore:'+score);
+      let textVie ='❤️❤️❤️';
+      if(life==2)
+      {textVie='❤️❤️';}
+      if(life==1)
+      {textVie='❤️';}
+      if(life==0)
+      {textVie='';}
+      let text = 'Timer: '+timeCount+'s\nHP:'+textVie+'\nScore: '+score+'\nHigh score: '+highScore;
+      this.chronoText.setText(text);
       let cursors = this.input.keyboard.createCursorKeys();
       if ((cursors.left.isDown || this.q.isDown) || (cursors.right.isDown || this.d.isDown)) 
-      {this.player.setVelocityX(cursors.left.isDown || this.q.isDown ? -270 : 270);}
+      {this.player.setVelocityX(cursors.left.isDown || this.q.isDown ? -300 : 300);}
       else 
       {this.player.setVelocityX(0);}
-    
-      this.obstacle.setVelocityY(speedObstacle);
+      
+      if(OursinActive)
+      {this.oursin.setVelocityY(speedObstacle);}
+      else
+      {this.oursin.setVelocityY(0);}
+      if(HarponActive)
+      {this.harpon.setVelocityY(speedObstacle);}
+      else
+      {this.harpon.setVelocityY(0);}
+      
       this.enfant.setVelocityY(speedEnfant);
       
-      if(this.obstacle.y > config.height+(this.obstacle.displayWidth/2))
+      if(this.oursin.y > config.height+(this.oursin.displayWidth/2) ||this.harpon.y > config.height+(this.oursin.displayWidth/2))
       {
-        this.obstacle.setPosition(RandInt(this.obstacle.displayWidth/2,config.width - (this.obstacle.displayWidth/2)), -50);
+        do{this.oursin.setPosition(RandInt(this.oursin.displayWidth/2,config.width - (this.oursin.displayWidth/2)), -150);}
+        while((this.oursin.x-(this.oursin.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.oursin.x-(this.oursin.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2)) 
+            || (this.oursin.x+(this.oursin.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.oursin.x+(this.oursin.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2)));
+    
+      do{this.harpon.setPosition(RandInt(this.harpon.displayWidth/2,config.width - (this.harpon.displayWidth/2)), -150);}
+        while((this.harpon.x-(this.harpon.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.harpon.x-(this.harpon.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2)) 
+            || (this.harpon.x+(this.harpon.displayWidth/2) > this.enfant.x-(this.enfant.displayWidth/2) 
+            && this.harpon.x+(this.harpon.displayWidth/2) < this.enfant.x+(this.enfant.displayWidth/2)));
+        let randSelect = RandInt(0,2);
+        if(randSelect==0)
+        {
+          HarponActive = true;
+          OursinActive = false;
+        }
+        if(randSelect==1)
+        {
+          HarponActive = false;
+          OursinActive = true;
+        }
         speedObstacle += 10;
       }
 
       if(this.enfant.y > config.height+(this.enfant.displayWidth/2))
       {
-        this.enfant.setPosition(RandInt(this.enfant.displayWidth/2,config.width - (this.enfant.displayWidth/2)), -50);
+        do{this.enfant.setPosition(RandInt(this.enfant.displayWidth/2,config.width - (this.enfant.displayWidth/2)), -50);}
+        while(((this.enfant.x-(this.enfant.displayWidth/2) > this.harpon.x-(this.harpon.displayWidth/2) 
+            && this.enfant.x-(this.enfant.displayWidth/2) < this.harpon.x+(this.harpon.displayWidth/2))
+            || ((this.enfant.x+(this.enfant.displayWidth/2) > this.harpon.x-(this.harpon.displayWidth/2) 
+            && this.enfant.x+(this.enfant.displayWidth/2) < this.harpon.x+(this.harpon.displayWidth/2))))
+            
+            || ((this.enfant.x-(this.enfant.displayWidth/2) > this.oursin.x-(this.oursin.displayWidth/2) 
+            && this.enfant.x-(this.enfant.displayWidth/2) < this.oursin.x+(this.oursin.displayWidth/2)) 
+            || (this.enfant.x+(this.enfant.displayWidth/2) > this.oursin.x-(this.oursin.displayWidth/2) 
+            && this.enfant.x+(this.enfant.displayWidth/2) < this.oursin.x+(this.oursin.displayWidth/2))));
+    
+        
         speedEnfant += 5;
         life -= 1;
       }
@@ -129,12 +249,16 @@ function update() {
       {
         this.win.setText('You lose');  
         pause = true;
-        this.obstacle.setVelocityY(0);
+        this.oursin.setVelocityY(0);
         this.player.setVelocityX(0);
         this.enfant.setVelocityY(0);
-        this.obstacle.setPosition(RandInt(this.obstacle.displayWidth/2,config.width - (this.obstacle.displayWidth/2)), -50);
+        this.harpon.setVelocityY(0);
+        this.oursin.setPosition(RandInt(this.oursin.displayWidth/2,config.width - (this.oursin.displayWidth/2)), -150);
         this.player.setPosition(config.width / 2, config.height - (this.player.displayHeight/2));
         this.enfant.setPosition(RandInt(this.enfant.displayWidth/2,config.width - (this.enfant.displayWidth/2)), -50);
+        this.harpon.setPosition(RandInt(this.harpon.displayWidth/2,config.width - (this.harpon.displayWidth/2)), -150);
+        if(score > highScore)
+        {highScore=score;}
       }
       
       if(life <= 0)
@@ -146,8 +270,7 @@ function update() {
       {
         this.win.setText(); 
       }
-    }
- 
+    } 
 }
 
 function onEvent()
@@ -155,22 +278,10 @@ function onEvent()
 
 function RandInt(min, max)
 {
-  Math.round(min);
-  Math.round(max);
-  return Math.random() * (max - min) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 }
-
-
-
-function reset(PauseOrNot)
-  {
-    pause = PauseOrNot;
-    this.obstacle.setVelocityY(0);
-    this.player.setVelocityX(0);
-    this.obstacle.setPosition(RandInt(this.obstacle.displayWidth/2,config.width - (this.obstacle.displayWidth/2)), -50);
-    this.player.setPosition(config.width / 2, config.height - (this.player.displayHeight/2));
-  }
-
 
 const config = {
     type: Phaser.AUTO,
